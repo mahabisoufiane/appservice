8fdce7f9ae1f5dc28c445791280275c4f36ba248
unknown <mahabisoufiane@gmail.com>
2021-01-01 15:07:03 +0100

    first commit

---
+++ gateway/src/main/java/org/sid/gatewayservice/GatewayServiceApplication.java
@@ -0,0 +1,32 @@
+package org.sid.gatewayservice;
+
+import com.netflix.discovery.DiscoveryClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@SpringBootApplication
+public class GatewayServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayServiceApplication.class, args);
+    }
+
+
+
+    @Bean
+    DiscoveryClientRouteDefinitionLocator definitionLocator(
+            ReactiveDiscoveryClient rdc,
+            DiscoveryLocatorProperties properties){
+        return new DiscoveryClientRouteDefinitionLocator(rdc, properties);
+
+    }
+
+}
